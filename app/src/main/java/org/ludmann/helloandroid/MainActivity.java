4865d34bf5514c77bc6a806e3ced3d0833b7a4f1
Cornelius A. Ludmann <dev@cornelius-ludmann.de>
2020-06-02 13:25:48 +0000

    Add button that shows a toast message

--- app/src/main/java/org/ludmann/helloandroid/MainActivity.java
+++ app/src/main/java/org/ludmann/helloandroid/MainActivity.java
@@ -2,6 +2,8 @@ package org.ludmann.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayToastMessageAction(View v) {
+        toastMessage(getString(R.string.message));
+    }
+
+    protected void toastMessage(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
